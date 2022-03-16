715269b081520e92084f59df2b77de80fc4ada0d
ahnafwarith <corp.ahnafwarid@gmail.com>
2022-03-16 21:26:57 +0600

    fixed bugs

--- src/App.js
+++ src/App.js
@@ -2,22 +2,26 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const names = ['Ahnaf', 'Anika', 'Kamrul'];
   return (
-    <div className="App">
+    < div className="App" >
+      {
+        names.map(name => <li>{name}</li>)
+      }
       <Person></Person>
       <Person></Person>
       {/* New Component */}
       <Friend name="Jhanku" job="Saving Lives"></Friend>
       <Friend name="Panku"></Friend>
       <Friend name="Sir<3"></Friend>
-    </div>
+    </div >
   );
 }
 function Person(props) {
   console.log(props)
   return (
     <div className='jhanku'>
-      <h1>Jhankar Mahmud</h1>
+      <h1>Jhankar</h1>
       <p>Profession: Programmer</p>
     </div>
   )
